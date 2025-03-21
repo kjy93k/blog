f302ac8771960e31e231513c907cb399866d203c
jy.kim <kjy93k@naver.com>
2025-03-22 00:05:45 +0900

    vault backup: 2025-03-22 00:05:45

--- quartz/components/RecentNotes.tsx
+++ quartz/components/RecentNotes.tsx
@@ -35,6 +35,7 @@ export default ((userOpts?: Partial<Options>) => {
     const opts = { ...defaultOptions(cfg), ...userOpts }
     const pages = allFiles.filter(opts.filter).sort(opts.sort)
     const remaining = Math.max(0, pages.length - opts.limit)
+
     return (
       <div class={classNames(displayClass, "recent-notes")}>
         <h3>{opts.title ?? i18n(cfg.locale).components.recentNotes.title}</h3>
