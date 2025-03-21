09125122b2df6977099f8e05e5188b6684203255
jy.kim <kjy93k@naver.com>
2025-03-22 00:20:45 +0900

    vault backup: 2025-03-22 00:20:45

--- check-commit-date.sh
+++ check-commit-date.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "✅ [hook] frontmatter.date 삽입 중..."
+
 # Git에서 커밋된 파일만 찾기
 for file in $(git diff --cached --name-only --diff-filter=A | grep '\.md$'); do
+  echo "☑️  Processing $file" # 파일 처리 시작
+
   if [[ -f "$file" ]]; then
-    echo "☑️  Processing $file"
+    echo "📄 파일이 존재: $file" # 파일이 존재하면 출력
 
     # 파일의 첫 번째 커밋 시간 확인
     created_date=$(git log --reverse --format=%aI -- "$file" | head -n 1)
@@ -14,5 +18,7 @@ for file in $(git diff --cached --name-only --diff-filter=A | grep '\.md$'); do
     else
       echo "📄 $file → Git 최초 커밋 날짜: $created_date"
     fi
+  else
+    echo "⚠️ $file → 존재하지 않는 파일"
   fi
 done
