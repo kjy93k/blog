c60cd293d29cf004113e4e471e233d12324f6157
jy.kim <kjy93k@naver.com>
2025-04-02 20:22:39 +0900

    vault backup: 2025-04-02 20:22:39

--- quartz.config.ts
+++ quartz.config.ts
@@ -58,7 +58,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter"],
+        priority: ["frontmatter", "git"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
